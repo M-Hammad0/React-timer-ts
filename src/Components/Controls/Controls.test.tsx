5ed1e62b371a43e37873807d1bd2f73e4c4a9ef4
M-Hammad0 <hammad_205@hotmail.com>
2020-08-11 00:46:13 +0500

    add workflow

--- src/Components/Controls/Controls.test.tsx
+++ src/Components/Controls/Controls.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import Control,{handlePlayButtonState} from './Controls'
+import Control from './Controls'
 import { render, cleanup } from '@testing-library/react';
 import setTimeInSeconds from '../Main/Main'
 
